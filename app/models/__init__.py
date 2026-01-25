8760d82537110018ca3083089769c91afa6e747e
Pranab Singh <mailps20@gmail.com>
2026-01-25 08:26:22 +0300

    Register FitScan model
    Ensure SQLAlchemy registry loads FitScan for relationship resolution.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from app.models.auth_magic_link_token import AuthMagicLinkToken
 from app.models.auth_refresh_token import AuthRefreshToken
+from app.models.fit_scan import FitScan
 from app.models.funding_opportunity import FundingOpportunity
 from app.models.ngo_profile import NGOProfile
 from app.models.usage_ledger import UsageLedger
@@ -9,6 +10,7 @@ from app.models.user_plan import UserPlan
 __all__ = [
     "AuthMagicLinkToken",
     "AuthRefreshToken",
+    "FitScan",
     "FundingOpportunity",
     "NGOProfile",
     "UsageLedger",
